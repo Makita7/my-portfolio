0f369b4fa3f3b28a6601103df7c90dae110c5766
Sofía Makita <sophiamakita@gmail.com>
2025-03-17 15:10:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import NavbarComp from './components/NavbarComp.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <NavbarComp />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+  <nav class="d-flex justify-space-between">
+    <p class="mb-0">MAKITA</p>
+    <div class="d-flex">
+      <RouterLink to="/" class="link pl-4">Home</RouterLink>
+      <RouterLink to="/about" class="link pl-4">About</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.link {
+  text-decoration: none,
+}
+</style>
