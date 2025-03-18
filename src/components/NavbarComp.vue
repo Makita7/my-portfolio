57aa0a8f90ada5f7bb64cf4812bc07f09102723f
Sofía Makita <sophiamakita@gmail.com>
2025-03-17 22:45:12 -0300

    navbar hover effect

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -6,24 +6,72 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <nav class="d-flex justify-space-between mb-4">
-    <p class="mb-0">MAKITA</p>
+    <img src="@/assets/makita-logo.png" alt="portfolio logo" class="logo" />
     <div class="d-flex">
       <TransitionGroup name="fade" mode="in-out">
-        <RouterLink to="/" class="link pl-4" activeClass="active" id="Home">Home</RouterLink>
-        <RouterLink to="/about" class="link pl-4" activeClass="active" id="About">About</RouterLink>
-        <RouterLink to="/about" class="link pl-4" activeClass="active" id="About">Projects</RouterLink>
-        <RouterLink to="/about" class="link pl-4" activeClass="active" id="About">CV</RouterLink>
+        <RouterLink to="/" class="link pl-4 home" activeClass="active" id="Home">Home</RouterLink>
+        <RouterLink to="/about" class="link pl-4 about" activeClass="active" id="About">About</RouterLink>
+        <RouterLink to="/about" class="link pl-4 project" activeClass="active" id="About">Projects</RouterLink>
+        <RouterLink to="/about" class="link pl-4 cv" activeClass="active" id="About">CV</RouterLink>
       </TransitionGroup>
     </div>
   </nav>
 </template>
 
 <style lang="scss" scoped>
+.logo {
+  height: 1.5rem;
+}
+
 .link {
   text-decoration: none;
   font-size: 18px;
   color: var(--blackish);
+
 }
+
+.home:hover {
+  color: var(--red);
+  font-weight: bold;
+  transition: all ease-in-out 0.2s;
+}
+
+.about:hover {
+  color: var(--green);
+  font-weight: bold;
+  transition: all ease-in-out 0.2s;
+}
+
+.project:hover {
+  color: var(--orange2);
+  font-weight: bold;
+  transition: all ease-in-out 0.2s;
+}
+
+.cv:hover {
+  color: #0069a2;
+  font-weight: bold;
+  transition: all ease-in-out 0.2s;
+}
+
+
+// .link:hover {
+//   animation: colorCycle 0.8s infinite alternate;
+// }
+
+// @keyframes colorCycle {
+//   33% {
+//     color: var(--red);
+//   }
+
+//   66% {
+//     color: var(--green);
+//   }
+
+//   100% {
+//     color: var(--orange2);
+//   }
+// }
 
 .active {
   text-decoration: underline;
