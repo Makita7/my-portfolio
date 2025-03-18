971ad3bfbbd2db477533d44c971579689caebdd6
Sofía Makita <sophiamakita@gmail.com>
2025-03-17 22:14:25 -0300

    footer icons hover

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -4,14 +4,14 @@
 <template>
   <footer class="d-flex justify-end mt-4">
     <p class="talk mb-0">Let's Talk</p>
-    <a href="https://www.behance.net/sophiamak">
+    <a href="https://www.behance.net/sophiamak" class="filter behance">
       <img src="@/assets/icons/behance.svg" alt="behance link" class="pl-4" />
     </a>
-    <a href="https://github.com/Makita7">
+    <a href="https://github.com/Makita7" class="filter github">
       <img src="@/assets/icons/github.svg" alt="github link" class="pl-4" />
     </a>
-    <a href="https://dribbble.com/SofiMakita">
-      <img src="@/assets/icons/dribble.svg" alt="dribble link" class="pl-4" />
+    <a href="https://dribbble.com/SofiMakita" class="filter dribble">
+      <img type="svg" src="@/assets/icons/dribble.svg" alt="dribble link" class="pl-4" />
     </a>
   </footer>
 </template>
@@ -27,5 +27,21 @@ footer {
 .talk {
   font-family: "Nunito", sans-serif;
   font-size: 1.5rem;
+}
+
+.filter img {
+  transition: filter 0.3s ease-in-out;
+}
+
+.dribble:hover img {
+  filter: invert(36%) sepia(76%) saturate(600%) hue-rotate(310deg);
+}
+
+.github:hover img {
+  filter: invert(28%) sepia(55%) saturate(4%) hue-rotate(21deg) brightness(96%) contrast(90%);
+}
+
+.behance:hover img {
+  filter: invert(22%) sepia(79%) saturate(6545%) hue-rotate(220deg) brightness(104%) contrast(103%);
 }
 </style>
